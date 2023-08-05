aada91681f72541d9ed97773191ba8ead1f22a2f
Asep Purnama <aseppurnama67@gmail.com>
2023-08-05 23:47:13 +0700

    working with js-1

---
+++ belajar.js
@@ -0,0 +1,37 @@
+// PENGENALAN TIPE DATA
+// console.log("Hello World")
+
+// const myHeading = document.querySelector("h1")
+// myHeading.textContent = "Hello World!"
+
+// let myVariable = null;
+// console.log(myVariable)
+
+// let payung;
+// console.log(payung)
+// console.log(typeof(payung))
+
+// let indonesia = true;
+// console.log(indonesia)
+
+// let angka = 1 === 1
+// console.log(angka)
+
+// let angka1 = 1
+// let angka2 = 5
+// console.log(typeof(angka1), typeof(angka2))
+
+// VARIABEL LET, CONST, & VAR
+// VAR VARIABLE = Global variable
+
+// LET and CONST VARIABLE = block-scope local variable. The different is Let Variable replaceable when redifine while const is unreplaceable
+let nama = "Bambang"
+console.log(nama)
+
+nama = "Asep"
+console.log(nama)
+
+const phi = 3.14;
+console.log(phi)
+
+phi = 4
