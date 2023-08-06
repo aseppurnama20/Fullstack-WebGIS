d986082f4c4b2bb14e80deb255ed51b7f5b2d99d
Asep Purnama <aseppurnama67@gmail.com>
2023-08-06 16:10:22 +0700

    day-2

--- belajar.js
+++ belajar.js
@@ -25,13 +25,60 @@
 // VAR VARIABLE = Global variable
 
 // LET and CONST VARIABLE = block-scope local variable. The different is Let Variable replaceable when redifine while const is unreplaceable
-let nama = "Bambang"
-console.log(nama)
+// let nama = "Bambang"
+// console.log(nama)
 
-nama = "Asep"
-console.log(nama)
+// nama = "Asep"
+// console.log(nama)
 
-const phi = 3.14;
-console.log(phi)
+// const phi = 3.14;
+// console.log(phi)
 
-phi = 4
+// const myImage = document.querySelector("img");
+
+// // JAVASCRIPT STANDARD
+// console.log('Asep')
+// console.log("Geocourse.id") //hindari
+
+// const jakarta = 'Ibukota Negara'
+// const bandung = 'Ibukota Provinsi'
+// const cibinong = 'Ibukota Kabupaten'
+
+// console.log('terdapat status' + ' ' + jakarta + ', ' + bandung + ', ' + cibinong)
+
+// let kota = 'jakarta'
+// let suhu = 25
+
+// const myFunction = function(){
+//     console.log(kota)
+// }
+
+// ARRAY
+const buah = ['apel', 'jeruk', 'durian']
+console.log(buah) 
+console.log(buah[0])
+
+buah.push('sirsak')
+console.log(buah)
+
+// OBJECT
+const profileRealMadrid = {
+    liga: 'La Liga',
+    peringkatLiga: 3,
+    negara: 'Spanyol',
+    pelatih: 'Ancelotti',
+    staion: 'Santiago Bernabeu'
+}
+
+console.log(profileRealMadrid)
+
+profileRealMadrid.sponsor = 'Fly Emirates'
+console.log(profileRealMadrid)
+
+const panitiaAgustusa = [
+    ['Andi', 'Warga', 24, true],
+    ['Budi', 'Warga', 28, true],
+    ['Putri', 'Warga', 22, false]
+]
+
+// console.log(panitiaAgustusan[0][3])
