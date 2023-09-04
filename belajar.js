0f3d21702a4cc5bb7731107432f63a47d5e7a684
Asep Purnama <aseppurnama67@gmail.com>
2023-09-04 22:05:48 +0700

    Update

--- belajar.js
+++ belajar.js
@@ -554,5 +554,84 @@
 // tombolTurun.addEventListener('click', () => ubahLogo(angka--))
 // tombolTurun.onclick = () => ubahLogo(angka--)
 
-// 📖 WebAPI JavaScript
-console.log('hello world')
+// 📖 WebAPI JavaScript - CONSOLE
+// console.log('hello world')
+// console.clear()
+// console.error('terjadi kesalahan kode')
+
+// 📖 WebAPI JavaScript - VALIDATION
+const divisi = document.createElement('div')
+document.body.appendChild(divisi)
+
+const judul = document.createElement('h1')
+judul.innerHTML = 'Ujicoba Validasi'
+divisi.appendChild(judul)
+
+const label = document.createElement('p')
+label.innerHTML = 'Masukkan nilai lintang'
+divisi.appendChild(label)
+
+const pilihan = document.createElement('select')
+pilihan.name = 'poros-koordinat'
+pilihan.id = 'poros-koordinat'
+divisi.appendChild(pilihan)
+
+const defaultValue = document.createElement('option')
+defaultValue.value = '-- Pilih kategori'
+defaultValue.innerHTML = '-- Pilih kategori'
+defaultValue.defaultSelected = true
+pilihan.appendChild(defaultValue)
+
+const lintang = document.createElement('option')
+lintang.value = 'Lintang'
+lintang.innerHTML = 'Lintang'
+pilihan.appendChild(lintang)
+
+const bujur = document.createElement('option')
+bujur.value = 'Bujur'
+bujur.innerHTML = 'Bujur'
+pilihan.appendChild(bujur)
+
+const inputData = document.createElement('input')
+inputData.type = 'number'
+divisi.appendChild(inputData)
+
+const tombolSubmit = document.createElement('button')
+tombolSubmit.type = 'submit'
+tombolSubmit.innerHTML = 'Kirim'
+divisi.appendChild(tombolSubmit)
+
+const pesanError = document.createElement('p')
+// pesanError.innerHTML = 'Hello World'
+divisi.appendChild(pesanError)
+
+console.log(document.querySelector('select > option[selected'))
+
+
+function validasi(){
+    // LOGIC
+    // Lintang = min(-90), max(90)
+    // Bujur = min(-180), max(180)
+
+    pilihan.onchange = function(){
+        const opsi = this.value
+        if(opsi === 'Lintang'){
+            inputData.min = 0
+            inputData.max = 90
+        } else {
+            inputData.min = 0
+            inputData.max = 180 
+        }
+        
+        if(inputData.checkValidity() && opsi == 'Lintang'){
+            return pesanError.innerHTML = `Data Valid, dipilih lintang`
+        } else if(inputData.checkValidity() && opsi == 'Bujur'){
+            return pesanError.innerHTML = `Data valid, dipilih bujur`
+        } else {
+            return pesanError.innerHTML = `Data tidak valid`
+        }
+    }
+}
+
+tombolSubmit.onclick = () => validasi()
+
